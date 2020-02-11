e9cd9f39aef5dc4938ab38faef8c6644333cfe2d
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 22:03:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  breakfast = ["eggs", "tea"]
+  breakfast = ["eggs", "am"]
   breakfast[1]
 end
 
