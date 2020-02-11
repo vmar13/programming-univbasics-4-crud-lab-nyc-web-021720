a26681bf7d66befcc66ce8db7307ba6b652f6d14
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 21:46:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  family = ["Maria", "Javier", "Jeff", "Patti"]
+  family << "Tio"
 end
 
 def add_element_to_start_of_array(array, element)
