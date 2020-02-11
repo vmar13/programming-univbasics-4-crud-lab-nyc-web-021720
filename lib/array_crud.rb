cec3666d1cfc316b5d5fe30179f53b5b91341ab0
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 22:08:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  lunch = ["chicken", "rice", "arrays!"]
-  arrays!=lunch.pop
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays!=array.pop
 end
 
 def remove_element_from_start_of_array(array)
