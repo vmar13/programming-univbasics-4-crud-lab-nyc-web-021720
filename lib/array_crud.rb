313cb7079f643a03c5811d463686a2110c2aff26
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 22:05:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  snack = ["yogurt", "almonds"]
+  snack = ["wow", "I", "am", "really", "learning", "arrays!"]
   snack
 end
 
