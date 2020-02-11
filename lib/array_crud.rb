ca9e9e6de12cea40c1bc5bed59b3a98f4db4b4e5
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 21:56:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  family = ["Maria", "Javier", "Jeff", "Patti", "Tio"]
+  Maria=family.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
