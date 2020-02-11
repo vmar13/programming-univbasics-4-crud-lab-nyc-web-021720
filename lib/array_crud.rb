0630c66a494808fd15bb312db3bd4c06f1579696
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 22:18:48 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  array=[]
 end
 
 def create_an_array
-  
+  family = ["Maria", "Javier", "Jeff", "Patti"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  family = ["Maria", "Javier", "Jeff", "Patti"]
+  family << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  family = ["Maria", "Javier", "Jeff", "Patti"]
+  family.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  family = ["wow", "Maria", "Javier", "Jeff", "Patti", "Tio"]
+  wow=family.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  breakfast = ["eggs", "am"]
+  breakfast[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  snack = ["wow", "I", "am", "really", "learning", "arrays!"]
+  snack[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  snack = ["wow", "I", "am", "really", "learning", "arrays!"]
+  snack[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[4]="totally"
 end
