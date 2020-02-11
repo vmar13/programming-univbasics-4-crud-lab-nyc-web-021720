2b088091c4e255a049f00bd1ef2286f8c2644aa7
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 21:54:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  family = ["Maria", "Javier", "Jeff", "Patti", "arrays!"]
-  arrays!=family.pop
+  family = ["Maria", "Javier", "Jeff", "Patti", "Tio"]
+  Tio=family.pop
 end
 
 def remove_element_from_start_of_array(array)
